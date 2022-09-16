c28204e5b9e4c306e66897b181797fb26310afe3
Luca De Santis <lucaroma2705@gmail.com>
2022-09-16 21:50:36 +0200

    setup absolute paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, "src"),
+      style: path.resolve(__dirname, "src/style"),
+      components: path.resolve(__dirname, "src/components"),
+    },
+  },
+  plugins: [react()],
+});
